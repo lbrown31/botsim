efab3df984c633f7ee9d4162a2d2fe5bedce27a1
lbrown31 <lbrown31@uvm.edu>
2024-01-26 14:36:43 -0500

    done with hw 2

--- generate.py
+++ generate.py
@@ -1,7 +1,27 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxs.sdf")
+length = 1
+width = 1
+height = 1
+x = 0
+y = 0
+z = height / 2
+for row in range(5):
+    for col in range(5):
+        # Calculate the position for the current tower
+        newx = col * length
+        newy = row * width
+        newz = height / 2
+        newlength = length
+        newwidth = width
+        newheight = height
+        for _ in range(10):
+            
+            pyrosim.Send_Cube(name="Box", pos=[newx, newy, newz], size=[newlength, newwidth, newheight])
+            newlength *= 0.9
+            newwidth *= 0.9
+            newheight *= 0.9
+            newz += newheight
 
 pyrosim.End()
 
