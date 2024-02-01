9c6da7cebe51018294d03af39d2d3185c2891bb7
lbrown31 <lbrown31@uvm.edu>
2024-02-01 13:50:43 -0500

    hook for hw 3

--- generate.py
+++ generate.py
@@ -1,11 +1,34 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxs.sdf")
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0
 z = height / 2
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x-2, y+2, z], size=[length, width, height])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link0_Link1", parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link1_Link2", parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link2", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Joint(name = "Link2_Link3", parent= "Link2" , child = "Link3" , type = "revolute", position = [0,.5,.5])
+    pyrosim.Send_Cube(name="Link3", pos=[x, .5, 0], size=[length, width, height])
+
+
+
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
+
+'''
 for row in range(5):
     for col in range(5):
         # Calculate the position for the current tower
@@ -22,6 +45,7 @@ for row in range(5):
             newwidth *= 0.9
             newheight *= 0.9
             newz += newheight
+'''
 
-pyrosim.End()
+
 
